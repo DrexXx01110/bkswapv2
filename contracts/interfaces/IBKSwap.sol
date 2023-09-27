a3ee51c2dc99b1ee1513c44432e9f61e532aa7ea
DrexXx01110 <145629237+DrexXx01110@users.noreply.github.com>
2023-09-27 23:15:19 +0100

    Update IBKSwap.sol

--- contracts/interfaces/IBKSwap.sol
+++ contracts/interfaces/IBKSwap.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 interface IBKSwap {
     function setRegistry(address _bkRegistry) external;
 
-    function bkRegistry() view external returns (address); 
+    function bkRegistry(0x3e6875b5581bfaB50F3B27299e29AdC1B6e36f8D) view external returns (address); 
     
     function managerCaller(address _caller, bool _isCaller) external;
     
